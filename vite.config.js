297a3b5ec829151830592fc18b22709d1f11ef21
KIDZU <alex.vebitsky@mail.ru>
2026-08-16 12:46:20 +0300

    Fix: dynamic base path for dev and prod

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/psych-sport-site/', // единственная важная строчка
+  // Если запускаем локально - убираем base, если на GitHub - добавляем
+  base: process.env.NODE_ENV === 'production' ? '/psych-sport-site/' : '/',
 })
